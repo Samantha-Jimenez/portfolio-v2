772af3fb0a28aecb60e0887d9086cbac0893e02d
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2024-12-21 08:05:45 -0500

    update fonts

--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,14 @@ const About = () => {
     return (
         <div className="container">
             <div className="section-title" data-aos="fade-left" data-aos-duration="1500">
-                <h2 className="sora-main">About</h2>
+                <h2 className="">About</h2>
                 <p>Brooklyn-born Afro-Latina. Builder. Creator. Problem Solver.</p>
             </div>
             <div className="row">
                 {/* <div className="col-lg-4 aos-init aos-animate" data-aos="fade-right" style={{"height": "30vh", "overflow": "hidden", "marginLeft": "15px", "marginRight": "15px"}}>
                 </div> */}
                 <div className="col-lg-auto pt-4 pt-lg-0 content aos-init aos-animate">
-                    <h3 className="sora-main" data-aos="fade-left" data-aos-duration="1000">Welcome to my Portfolio</h3>
+                    <h3 className="" data-aos="fade-left" data-aos-duration="1000">Welcome to my Portfolio</h3>
                     <p className="fst-italic pt-4" data-aos="fade-left" data-aos-duration="1000">
                         I’m a Full Stack Software Engineer passionate about crafting beautiful, functional applications that make an impact. My coding journey began in 2018, and by 2019, I joined Pursuit, a renowned coding fellowship in Queens, NY, where I turned curiosity into code and ideas into reality.                    
                     </p>
@@ -22,12 +22,12 @@ const About = () => {
                     {/* <p style={{"fontWeight": "300"}} data-aos="fade-left" data-aos-duration="1000">
                         Welcome to my portfolio! 
                     </p> */}
-                    <p style={{"fontWeight": "300"}} data-aos="fade-left" data-aos-duration="1000">
+                    <p data-aos="fade-left" data-aos-duration="1000">
                         For over three years, I honed my skills at Peloton, playing a pivotal role in enhancing their membership platform and delivering seamless user experiences to a global audience. Alongside my engineering career, I earned a degree in Business, Management, & Economics from Empire State College, blending technical expertise with business strategy.
                         Now, I’m on a mission to keep building, learning, and collaborating on projects that push boundaries and spark innovation.
                     </p>
                     <br />
-                    <p style={{"fontWeight": "300"}} data-aos="fade-left" data-aos-duration="1000">
+                    <p data-aos="fade-left" data-aos-duration="1000">
                         Welcome to my portfolio—explore my work, and let’s connect to create something incredible together!
                     </p>
                 </div>
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,8 +8,8 @@ const Landing = () => {
             <section id="hero" className="d-flex flex-column justify-content-end h-screen ml-auto bg-[#EFF2F1] p-0">
                 <div id="backgroundImg"></div>
                 <div className="hero-container aos-init aos-animate" data-aos="fade-left">
-                    <h1 className="space-grotesk-main">Samantha Jimenez</h1>
-                    <p style={{"display": "inline-flex"}}>I'm a <span style={{"borderBottom": "3px solid var(--attention)"}} className="typed" >
+                    <h1 className="">Samantha Jimenez</h1>
+                    <p className="inter-main" style={{"display": "inline-flex"}}>I'm a <span style={{"borderBottom": "3px solid var(--attention)"}} className="typed inter-main" >
                         <TypeAnimation
                             sequence={[
                                 'Full Stack Software Engineer',
