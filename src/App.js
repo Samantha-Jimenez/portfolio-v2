94160e3a24c1383bb8e6bd2e71f8a7f0dffc3d42
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2024-12-28 08:20:39 -0500

    fix bug to show all footer and contact form

--- src/App.js
+++ src/App.js
@@ -136,7 +136,7 @@ function App() {
                 </svg>
               </button>
             </div>
-          <main id="main" className="grid lg:grid-cols-[30vw_70vw] lg:grid-rows-[0.4fr_0.75fr_max-content_0.75fr_0.5fr] grid-cols-[0vw_100vw]">
+          <main id="main" className="grid lg:grid-cols-[30vw_70vw] lg:grid-rows-[0.4fr_0.75fr_max-content_0.75fr_fit-content[100%]] grid-cols-[0vw_100vw]">
             <header id="header-back" className={`py-8 px-4 lg:w-[30vw] w-[307px] h-screen bg-[url('./imgFiles/avatar.jpeg')] bg-cover bg-center fixed grid-area-1/1/2/2 z-10 lg:block ${isMenuOpen ? 'block' : 'hidden'}`}></header>
             <Element id="landing" name="landing"><Landing/></Element>
             <button className="down-arrow hidden lg:block z-30 h-[5vh] whit mb-[35%]">
@@ -210,7 +210,7 @@ function App() {
             <section id="contact" className="contact pt-20">
               <Element name='contact'><Contact/></Element>
               <footer id='footer' data-aos="fade-up">
-                <p className='credits pt-5'>
+                <p className='credits pt-8'>
                 Thanks for visiting my corner of the web! 
                 <br></br>
                 Keep exploring, keep creating, and remember — every line of code brings you closer to your next big idea. 🚀
