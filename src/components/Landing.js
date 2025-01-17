451fd82fc995a9d3c6769b3f331481d354426120
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-01-16 23:18:46 -0500

    update styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ const Landing = () => {
     return (
         <div>
             <section id="hero" className="d-flex flex-column justify-content-end h-screen ml-auto bg-[#EFF2F1] p-0 section">
-                <div id="backgroundImg"></div>
+                <div id="backgroundImg" className="w-full h-screen"></div>
                 <div className="hero-container aos-init aos-animate" data-aos="fade-left">
                     <h1 className="">Samantha Jimenez</h1>
                     <p className="inter-main" style={{"display": "inline-flex"}}>I'm a <span style={{"borderBottom": "3px solid var(--button-selected)"}} className="typed inter-main" >
