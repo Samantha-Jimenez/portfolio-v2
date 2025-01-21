2889eecb34a3982bfce95d53d2521b45f469a8f7
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-01-20 23:33:21 -0500

    remove font family from timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -102,7 +102,7 @@ const Timeline = () => {
             <div className="-my-6">
                 {/* Mvmnt Collectives */}
                 <div className="relative pl-8 sm:pl-32 py-6 group">
-                    <div className="font-medium text-2xl text-green-600/55 mb-1 sm:mb-0 font-['Sora']" data-aos="fade-left" data-aos-duration="1500">Mvmnt Collectives</div>
+                    <div className="font-medium text-2xl text-green-600/55 mb-1 sm:mb-0" data-aos="fade-left" data-aos-duration="1500">Mvmnt Collectives</div>
                     <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-[var(--button-selected)] after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                         <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-[var(--background-secondary)] rounded-full shadow-lg" data-aos="fade-right" data-aos-duration="1500">2024 - '25</time>
                         <div className="text-xl font-bold text-[var(--underline-and-button-color)]" data-aos="fade-left" data-aos-duration="1500">Running Club App</div>
@@ -159,7 +159,7 @@ const Timeline = () => {
 
                 {/* Othello */}
                 <div className="relative pl-8 sm:pl-32 py-6 group">
-                    <div className="font-medium text-2xl text-green-600/55 mb-1 sm:mb-0 font-['Sora']" data-aos="fade-left" data-aos-duration="1500">Othello</div>
+                    <div className="font-medium text-2xl text-green-600/55 mb-1 sm:mb-0" data-aos="fade-left" data-aos-duration="1500">Othello</div>
                     <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-[var(--button-selected)] after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                         <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-[var(--background-secondary)] rounded-full shadow-lg" data-aos="fade-right" data-aos-duration="1500">2024</time>
                         <div className="text-xl font-bold text-[var(--underline-and-button-color)]" data-aos="fade-left" data-aos-duration="1500">Pursuit Hackathon Award-Winning App for Most Innovative Use of AI</div>
@@ -216,7 +216,7 @@ const Timeline = () => {
 
                 {/* Peloton */}
                 <div className="relative pl-8 sm:pl-32 py-6 group">
-                    <div className="font-medium text-2xl text-green-600/55 mb-1 sm:mb-0 font-['Sora']" data-aos="fade-left" data-aos-duration="1500">Peloton Interactive, Inc.</div>
+                    <div className="font-medium text-2xl text-green-600/55 mb-1 sm:mb-0" data-aos="fade-left" data-aos-duration="1500">Peloton Interactive, Inc.</div>
                     <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-[var(--button-selected)] after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                         <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-[var(--background-secondary)] rounded-full shadow-lg" data-aos="fade-right" data-aos-duration="1500">2021 - '23</time>
                         <div className="text-xl font-bold text-slate-900" data-aos="fade-left" data-aos-duration="1500">Peloton Interactive, Inc.</div>
@@ -252,7 +252,7 @@ const Timeline = () => {
 
                 {/* ZooVio */}
                 <div className="relative pl-8 sm:pl-32 py-6 group">
-                    <div className="font-medium text-2xl text-green-600/55 mb-1 sm:mb-0 font-['Sora']" data-aos="fade-left" data-aos-duration="1500">ZooVio</div>
+                    <div className="font-medium text-2xl text-green-600/55 mb-1 sm:mb-0" data-aos="fade-left" data-aos-duration="1500">ZooVio</div>
                     <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-[var(--button-selected)] after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                         <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-[var(--background-secondary)] rounded-full shadow-lg" data-aos="fade-right" data-aos-duration="1500">2019 - '20</time>
                         <div className="text-xl font-bold text-slate-900" data-aos="fade-left" data-aos-duration="1500">Capstone Project at Pursuit Coding Fellowship</div>
@@ -291,7 +291,7 @@ const Timeline = () => {
 
                 {/* Finstagram */}
                 <div className="relative pl-8 sm:pl-32 py-6 group">
-                    <div className="font-medium text-2xl text-green-600/55 mb-1 sm:mb-0 font-['Sora']" data-aos="fade-left" data-aos-duration="1500">Finstagram</div>
+                    <div className="font-medium text-2xl text-green-600/55 mb-1 sm:mb-0" data-aos="fade-left" data-aos-duration="1500">Finstagram</div>
                     <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-[var(--button-selected)] after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                        <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-[var(--background-secondary)] rounded-full shadow-lg" data-aos="fade-right" data-aos-duration="1500">2019 - '20</time>
                        <div className="text-xl font-bold text-slate-900" data-aos="fade-left" data-aos-duration="1500">Independent Project at Pursuit Coding Fellowship</div>
@@ -330,7 +330,7 @@ const Timeline = () => {
 
                 {/* Tastebuds */}
                 <div className="relative pl-8 sm:pl-32 py-6 group">
-                    <div className="font-caveat font-medium text-2xl text-green-600/55 mb-1 sm:mb-0 font-['Sora']" data-aos="fade-left" data-aos-duration="1500">Tastebuds</div>
+                    <div className="font-caveat font-medium text-2xl text-green-600/55 mb-1 sm:mb-0" data-aos="fade-left" data-aos-duration="1500">Tastebuds</div>
                     <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-[var(--button-selected)] after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
                         <time className="sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 text-emerald-600 bg-[var(--background-secondary)] rounded-full shadow-lg" data-aos="fade-right" data-aos-duration="1500">2019 - '20</time>
                         <div className="text-xl font-bold text-slate-900" data-aos="fade-left" data-aos-duration="1500">Codecademy Sponsored Pursuit Hackathon Award-Winning App for Most Creative</div>
