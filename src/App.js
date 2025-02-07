11a6b7abaaa286474bd99293ed50a2954b7375c5
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-02-07 13:58:14 -0500

    comment out old resume

--- src/App.js
+++ src/App.js
@@ -328,12 +328,12 @@ function App() {
                     <span className="icon-[bx--mail-send] mr-2"></span>Contact
                   </Link>
                 </li>
-//                 <li data-aos={windowWidth > 1023 ? "fade-up" : undefined}>
-//                   <a className="menuLink" href="https://drive.google.com/file/d/1mLEMcUxuJGYWjr4ebNv7zvZY6Yo-RS_w/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-//                     <span className="icon-[tabler--file-text] mr-2"></span>
-//                     <span style={{"marginLeft": "-4px"}}>Resume</span>
-//                   </a>
-//                 </li>
+                 {/* <li data-aos={windowWidth > 1023 ? "fade-up" : undefined}>
+                   <a className="menuLink" href="https://drive.google.com/file/d/1mLEMcUxuJGYWjr4ebNv7zvZY6Yo-RS_w/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                     <span className="icon-[tabler--file-text] mr-2"></span>
+                     <span style={{"marginLeft": "-4px"}}>Resume</span>
+                   </a>
+                 </li> */}
                 <li data-aos={windowWidth > 1023 ? "fade-up" : undefined}>
                   <a className="menuLink" href="https://www.canva.com/design/DAGdIEMAtNo/9Ogf8MbGWa3s1OrTmh8cqA/view?utm_content=DAGdIEMAtNo&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h856a94e905" target="_blank" rel="noopener noreferrer">
                     <span className="icon-[tabler--file-text] mr-2"></span>
