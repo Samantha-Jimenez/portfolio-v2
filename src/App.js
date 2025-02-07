b91602a74c4c4cd50b3126cd215e4bc4d312929f
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-02-06 19:13:39 -0500

    remove focus outline on chevron

--- src/App.js
+++ src/App.js
@@ -225,7 +225,7 @@ function App() {
           <main id="main" className="grid lg:grid-cols-[30vw_70vw] grid-rows-[0.4fr_max-content_max-content_0.75fr_max-content] grid-cols-[0vw_100vw]">
             <header id="header-back" className={`py-8 px-4 min-[1026px]:w-[30vw] w-[290px] h-screen bg-[url('./imgFiles/81563B65-38C8-49D2-98A2-36CD0ADBB84D_1_105_c.jpeg')] bg-cover bg-center fixed grid-area-1/1/2/2 z-10 lg:block ${isMenuOpen ? 'block' : 'hidden'}`}></header>
             <Element id="landing" name="landing"><Landing/></Element>
-            <button className="down-arrow hidden lg:block z-30 h-[5vh] whit mb-[35%]">
+            <button className="down-arrow hidden lg:block z-30 h-[5vh] white mb-[35%] focus:outline-none">
               <Link to='about' className="linkA" activeClass='activeNav' spy={true} smooth={true} duration={1200} offset={offset}>
                 <span className='icon-[line-md--chevron-down] w-[60px] h-[100%] animate-bounce bg-white opacity-[.80]'/>
               </Link>
