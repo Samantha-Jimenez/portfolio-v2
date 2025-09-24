b4443855eec7076dec7807574fc65c898706f9fc
Samantha <samanthab.jimenez@gmail.com>
2025-09-24 13:58:43 -0400

    nav menu height responsive

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ function App() {
                 )}
               </button>
             </div>
-          <main id="main" className="grid lg:grid-cols-[30vw_70vw] grid-rows-[0.4fr_max-content_max-content_0.75fr_max-content] grid-cols-[0vw_100vw]">
+          <main id="main" className="grid lg:grid-cols-[30vw_70vw] grid-rows-[0.4fr_max-content_max-content_0.75fr_fit-content] grid-cols-[0vw_100vw]">
             <header id="header-back" className={`py-8 px-4 min-[1026px]:w-[30vw] w-[290px] h-screen bg-[url('./imgFiles/81563B65-38C8-49D2-98A2-36CD0ADBB84D_1_105_c.jpeg')] bg-cover bg-center fixed grid-area-1/1/2/2 z-10 lg:block ${isMenuOpen ? 'block' : 'hidden'}`}></header>
             <Element id="landing" name="landing"><Landing isMenuOpen={isMenuOpen} /></Element>
             <button className="down-arrow hidden lg:block z-30 h-[5vh] white mb-[35%] focus:outline-none w-[30vw]">
@@ -200,7 +200,7 @@ function App() {
                 <span className='icon-[line-md--chevron-down] w-[60px] h-[100%] animate-bounce bg-white opacity-[.80]'/>
               </Link>
             </button>
-            <div id="header-hover" className={`sticky top-0 bg-[rgb(9,18,14)] opacity-80 py-[48px] px-4 min-[1026px]:w-[30vw] w-[290px] h-screen content-evenly z-20 lg:grid ${isMenuOpen ? 'grid' : 'hidden'}`}>
+            <div id="header-hover" className={`sticky top-0 bg-[rgb(9,18,14)] opacity-80 px-4 min-[1026px]:w-[30vw] w-[290px] h-screen content-evenly z-20 lg:grid ${isMenuOpen ? 'grid' : 'hidden'}`}>
             <div className="profile">
               <h1 className="header-title leading-[1.1] text-[var(--light-background-and-text-primary)]" data-aos={windowWidth > 1023 ? "fade-up" : undefined}>Samantha Jimenez</h1>
               <div className="social-links text-center">
