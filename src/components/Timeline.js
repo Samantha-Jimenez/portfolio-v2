e8a659a1d15b3a6578e79d03bbc16ca1528a10dc
Samantha <samanthab.jimenez@gmail.com>
2026-01-28 12:14:52 -0500

    update timeline years

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -154,7 +154,7 @@ const Timeline = () => {
                 <div className="relative pl-8 sm:pl-32 py-6 group">
                     <div className="timeline-header font-medium text-2xl mb-1 sm:mb-0" data-aos="fade-left" data-aos-duration="1500">Our Rootline</div>
                     <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-[rgb(178,204,62)] after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                        <time className="timeline-pill sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 rounded-full shadow-lg" data-aos="fade-right" data-aos-duration="1500">2025</time>
+                        <time className="timeline-pill sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 rounded-full shadow-lg" data-aos="fade-right" data-aos-duration="1500">2025 - '26</time>
                         <div className="timeline-subheader text-xl font-bold" data-aos="fade-left" data-aos-duration="1500">Family Tree & Photo Album App</div>
                     </div>
                     <div className="text-neutral-500" data-aos="fade-left" data-aos-duration="1500">
@@ -211,7 +211,7 @@ const Timeline = () => {
                 <div className="relative pl-8 sm:pl-32 py-6 group">
                     <div className="timeline-header font-medium text-2xl mb-1 sm:mb-0" data-aos="fade-left" data-aos-duration="1500">Motevis</div>
                     <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-[rgb(178,204,62)] after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
-                        <time className="timeline-pill sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 rounded-full shadow-lg" data-aos="fade-right" data-aos-duration="1500">2024 - '25</time>
+                        <time className="timeline-pill sm:absolute left-0 translate-y-0.5 inline-flex items-center justify-center text-xs font-semibold uppercase w-20 h-6 mb-3 sm:mb-0 rounded-full shadow-lg" data-aos="fade-right" data-aos-duration="1500">2024 - '26</time>
                         <div className="timeline-subheader text-xl font-bold" data-aos="fade-left" data-aos-duration="1500">Running Club App</div>
                     </div>
                     <div className="text-neutral-500" data-aos="fade-left" data-aos-duration="1500">
